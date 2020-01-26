3da59888972eb515843cfbd5d13adc4436876f12
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-01-26 07:58:51 +0530

    Added node_modules on exclude

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import { configureStore, history } from '@state/configureStore';
 import Root from './containers/Root';
-// import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
