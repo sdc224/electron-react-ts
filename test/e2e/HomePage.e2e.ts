b09dc94c712ba4f43cc1f8d22ad5199d60f34f4f
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-01-30 23:41:38 +0530

    Updated e2e tests

--- test/e2e/HomePage.e2e.ts
+++ test/e2e/HomePage.e2e.ts
@@ -9,8 +9,6 @@ const clickToCounterLink = t =>
   t.click(Selector('a').withExactText('to Counter'));
 const incrementButton = buttonsSelector.nth(0);
 const decrementButton = buttonsSelector.nth(1);
-const oddButton = buttonsSelector.nth(2);
-const asyncButton = buttonsSelector.nth(3);
 const getCounterText = () => counterSelector().innerText;
 const assertNoConsoleErrors = async t => {
   const { error } = await t.getBrowserConsoleMessages();
@@ -58,35 +56,11 @@ test('should display updated count after the increment button click', async t =>
     .eql('1');
 });
 
-test('should display updated count after the descrement button click', async t => {
+test('should display updated count after the decrement button click', async t => {
   await t
     .click(decrementButton)
     .expect(getCounterText())
     .eql('-1');
-});
-
-test('should not change even counter if odd button clicked', async t => {
-  await t
-    .click(oddButton)
-    .expect(getCounterText())
-    .eql('0');
-});
-
-test('should change odd counter if odd button clicked', async t => {
-  await t
-    .click(incrementButton)
-    .click(oddButton)
-    .expect(getCounterText())
-    .eql('2');
-});
-
-test('should change if async button clicked and a second later', async t => {
-  await t
-    .click(asyncButton)
-    .expect(getCounterText())
-    .eql('0')
-    .expect(getCounterText())
-    .eql('1');
 });
 
 test('should back to home if back button clicked', async t => {
