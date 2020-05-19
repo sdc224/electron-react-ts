f26e146c96e86beaaef399ca5e248eba7ae590c4
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-05-19 19:03:54 +0530

    Deleted unused gitlabHelper

--- app/state/utils/gitlabHelper.ts
+++
@@ -1,9 +0,0 @@
-import { Gitlab } from 'gitlab';
-import credentials from '@private/credentials';
-
-export const initialize = () => new Gitlab(credentials);
-
-export const getAllProjects = async () => {
-  const res = await initialize().Projects.all();
-  return res;
-};
