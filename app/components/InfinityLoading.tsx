ff1cfb2b3a48ebb3d5d3b42bdd56f21c3bc7d0b1
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-05-23 23:52:10 +0530

    Added Infinity Loading Component

---
+++ app/components/InfinityLoading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import infinityLoading from '@images/Infinity-1s-200px.svg';
+
+// interface IInfinityLoadingProps {}
+
+const InfinityLoading = () => {
+  return <img alt="Loading..." src={infinityLoading} />;
+};
+
+export default InfinityLoading;
