f428510fef3dbe23c89bc71f5a5c08cd7b4a7ab2
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-05-23 16:48:51 +0530

    Added back button in Not found route

--- app/assets/ts/styles/views/notFoundStyles.ts
+++ app/assets/ts/styles/views/notFoundStyles.ts
@@ -3,15 +3,26 @@ import { Theme, createStyles } from '@material-ui/core';
 export default (theme: Theme) =>
   createStyles({
     root: {
+      boxSizing: 'border-box',
+      width: '100%',
       padding: theme.spacing(4),
       height: '100%'
     },
     content: {
+      width: '100%',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
       textAlign: 'center'
+    },
+    contentHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      paddingTop: theme.spacing(5),
+      paddingBottom: theme.spacing(2),
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2)
     },
     image: {
       marginTop: 50,
--- app/views/NotFound/index.tsx
+++ app/views/NotFound/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Fab } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styles from '@viewsTSStyles/notFoundStyles';
 import pageNotFound from '@images/undraw_page_not_found_su7k.svg';
 
@@ -8,19 +10,31 @@ const useStyles = makeStyles(styles);
 
 const NotFound = () => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const handleBack = () => {
+    history.goBack();
+  };
 
   return (
     <div className={classes.root}>
       <Grid container justify="center" spacing={4}>
         <Grid item lg={6} xs={12}>
           <div className={classes.content}>
-            <Typography variant="h1">
-              404: The page you are looking for isn’t here
-            </Typography>
-            <Typography variant="subtitle2">
-              You either tried some shady route or you came here by mistake.
-              Whichever it is, try using the navigation
-            </Typography>
+            <div className={classes.contentHeader}>
+              <Fab size="small" onClick={handleBack}>
+                <ArrowBackIcon />
+              </Fab>
+              <div className={classes.content}>
+                <Typography variant="h1">
+                  404: The page you are looking for isn’t here
+                </Typography>
+                <Typography variant="subtitle2">
+                  You either tried some shady route or you came here by mistake.
+                  Whichever it is, try using the navigation
+                </Typography>
+              </div>
+            </div>
             <img
               alt="Under development"
               className={classes.image}
