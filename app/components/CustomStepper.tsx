5e0689aeec307a73469190cc19b01464c05156a8
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-06-20 21:42:58 +0530

    Added stepper

---
+++ app/components/CustomStepper.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Stepper, Step, StepLabel } from '@material-ui/core';
+
+interface ICustomStepperProps {
+  steps: string[];
+  activeStep: number;
+}
+
+const CustomStepper = ({ steps, activeStep = 0 }: ICustomStepperProps) => {
+  return (
+    <Stepper alternativeLabel activeStep={activeStep}>
+      {steps.map(label => (
+        <Step key={label}>
+          <StepLabel>{label}</StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  );
+};
+
+export default CustomStepper;
